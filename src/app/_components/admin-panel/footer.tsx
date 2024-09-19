3d39f3c9fb7a37b2681dc709766b7b7d4dc4de3a
Thomas <nyumat18@gmail.com>
2024-09-19 06:48:04 -0400

    💄 footer styling and copy

--- src/app/_components/admin-panel/footer.tsx
+++ src/app/_components/admin-panel/footer.tsx
@@ -2,21 +2,21 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <div className="z-20 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <div className="z-20 w-full bg-background shadow backdrop-blur border-t-[1px]">
       <div className="mx-4 flex h-14 items-center md:mx-8">
         <p className="text-left text-xs leading-loose text-muted-foreground md:text-sm">
-          Built on top of{" "}
+          SickleSense was made for the{" "}
           <Link
-            href="https://ui.shadcn.com"
+            href="https://afrotech.devpost.com/"
             target="_blank"
             rel="noopener noreferrer"
             className="font-medium underline underline-offset-4"
           >
-            shadcn/ui
+            AfroTech AI Hackathon
           </Link>
           . The source code is available on{" "}
           <Link
-            href="https://github.com/salimi-my/shadcn-ui-sidebar"
+            href="https://github.com/Nyumat/sicklesense"
             target="_blank"
             rel="noopener noreferrer"
             className="font-medium underline underline-offset-4"
