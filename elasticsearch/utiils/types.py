2081474ccf8154a49c6d2cda36ac60b6e546452b
Thomas <nyumat18@gmail.com>
2024-09-30 06:17:20 -0700

    🔗 pydantic models for experimental features

---
+++ elasticsearch/utiils/types.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class ClientAttachment(BaseModel):
+    name: str
+    contentType: str
+    url: str
+
+
+class ToolInvocation(BaseModel):
+    toolCallId: str
+    toolName: str
+    args: dict
+    result: dict
