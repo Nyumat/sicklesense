04fb63f72bd06f23adfabb2b889d4cc72d964559
Thomas <nyumat18@gmail.com>
2024-09-30 06:31:24 -0700

    ⌚️ device metrics page

---
+++ src/app/(router)/devices/metrics/page.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+import { ContentLayout } from "@/app/_components/admin-panel/content-layout";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { RealTimeDeviceMetrics } from "../(helper)/rt-device-metrics";
+
+export default function DeviceMetricsPage() {
+    return (
+        <ContentLayout title="Wearable Device Metrics">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/dashboard">Home</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/dashboard">Dashboard</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/devices">Devices</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>Device Metrics</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            <RealTimeDeviceMetrics />
+        </ContentLayout>
+    );
+}
