08f10d8eef51952d44a7728cf7f9441ff6194ced
Thomas <nyumat18@gmail.com>
2024-09-30 06:31:12 -0700

    📱 connect devices page

---
+++ src/app/(router)/devices/connect/page.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+import { ContentLayout } from "@/app/_components/admin-panel/content-layout";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import PlaceholderContent from "@/app/_components/place";
+
+export default function DeviceConnectPage() {
+    return (
+        <ContentLayout title="Connect a Device">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/dashboard">Home</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/dashboard">Dashboard</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                            <Link href="/devices">Devices</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>   
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem>
+                        <BreadcrumbPage>Connect a Device</BreadcrumbPage>
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            <PlaceholderContent />
+        </ContentLayout>
+    );
+}
