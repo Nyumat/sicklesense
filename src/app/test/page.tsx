ab4854f985096897e0bc2fd7ec57b18301ec2dce
Thomas <nyumat18@gmail.com>
2024-09-30 06:33:41 -0700

    👶🏾 stub structured completions response w/ streaming

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,45 +1,28 @@
 'use client';
 
-import { useChat } from 'ai/react';
-import { useSession } from 'next-auth/react';
-import { Card } from './custom-card';
+import { experimental_useObject as useObject } from 'ai/react';
+import { useState } from 'react';
+import { z } from 'zod';
+const questionsSchema = z.array(z.object({ id: z.string(), question: z.string() }));
 
 export default function Page() {
-    const { messages, input, handleSubmit, handleInputChange, isLoading } =
-        useChat({
-            api: 'http://localhost:8000/api/chat?protocol=text',
-            streamProtocol: 'text',
-            initialInput: '',
-            initialMessages: [
-                { id: '1', role: 'system', content: `Hello {Tom}! I\'m an A.I. chatbot fine tuned on over 25,000 sickle cell articles, books, and papers. Feel free to ask me anything.` },
-            ],
-        });
+    const [input, setInput] = useState('');
+    const { object, submit } = useObject({
+        api: '/api/questions',
+        schema: z.object({ questions: questionsSchema }),
+    });
 
     return (
-        <div className="flex flex-col gap-2">
-            <div className="flex flex-col p-4 gap-2">
-                {messages.map(message => (
-                    <div key={message.id} className="flex flex-row gap-2">
-                        <div className="w-24 text-zinc-500 flex-shrink-0">{`${message.role}: `}</div>
-                        <div className="flex flex-col gap-2">{message.content}</div>
+        <div>
+            <input value={input} onChange={(e) => setInput(e.target.value)} />
+            <button onClick={() => submit(input)}>Submit</button>
+            <div className="grid grid-cols-2 gap-4">
+                {object?.questions?.map((question) => (
+                    <div key={question?.id} className="p-4 border border-gray-200 rounded">
+                        <h3 className="font-bold">{question?.question}</h3>
                     </div>
                 ))}
             </div>
-
-            {messages.length === 0 && <Card type="chat-text" />}
-
-            <form
-                onSubmit={handleSubmit}
-                className="flex flex-col fixed bottom-0 w-full border-t"
-            >
-                <input
-                    value={input}
-                    placeholder="Why is the sky blue?"
-                    onChange={handleInputChange}
-                    className="w-full p-4 outline-none bg-transparent"
-                    disabled={isLoading}
-                />
-            </form>
         </div>
     );
 }
